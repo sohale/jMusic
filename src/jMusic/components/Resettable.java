82671ebc8207185088be936e46b03af5779b3c88
Sohail Siadat <sohale@gmail.com>
2014-10-29 03:17:57 +0000

    more

---
+++ src/jMusic/components/Resettable.java
@@ -0,0 +1,6 @@
+package jMusic.components;
+
+/**
+ * Resettable means the component has a state. It needs to be reset in the beginning. 
+ */
+public interface Resettable {void reset();};	//or firstSample? 
