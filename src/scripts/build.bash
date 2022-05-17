43c6d18ace03a75695ae180e4a4a3f4d1aa35d49
Sohail Siadatnejad <sohale@gmail.com>
2022-05-17 23:31:19 +0100

    some failed commmands to cnovert raw audio files

--- src/scripts/build.bash
+++ src/scripts/build.bash
@@ -3,6 +3,11 @@ set -eu
 REPOROOT=$(git rev-parse --show-toplevel)
 cd $REPOROOT
 
+# failed attempts to convert raw file:
+# brew install sox
+# brew install lame
+# sox -t raw -b 16  -c 2 -r 44100  OUT.raw out.wav
+# afconvert -v -d BEI16@44100   OUT.raw -o out.wav -f wav
 
 cd src
 find .|grep -e "\.class" |xargs rm
